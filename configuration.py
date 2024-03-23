ec7e2ef84d16adfb73ab3bc7a8cf8d0048e50494
leroyspb <leroy777@bk.ru>
2024-03-23 13:39:23 +0300

    Test init

---
+++ configuration.py
@@ -0,0 +1,14 @@
+import telebot
+TOKEN = '6320940118:********************'
+
+bot = telebot.TeleBot(TOKEN)
+# Crypto178Bot
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+
+}
